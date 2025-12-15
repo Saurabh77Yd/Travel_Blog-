e200544c995c1b67d5ad02055ab40e122086c15e
Saurabh Yadav <yadavsaurabhyd@gmail.com>
2025-12-15 12:14:37 +0530

    Registration and logn backend

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,97 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors  = require("cors");
-
 const jwt  = require ("jsonwebtoken");
 
-mongoose.connect(config.connectionString)
+const User = require("./models/user.model")
+
+mongoose.connect(config.connectionString);
+
+
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin:"*"}));
 
 //Create Account 
-app.get("/create-account", async(req, res)=>{
-    return res.status(200).json({message:"hello"})
+app.post("/create-account", async(req, res)=>{
+    const {fullName, email, password} = req.body;
+     if(!fullName|| !email || !password) {
+        return res.status(400).json({
+            error:true, message:"All fields are require"
+        })
+    }
+
+    const isUser = await User.findOne({email});
+    if(isUser){
+        return res.status(400).json({
+            error:true,
+            message:"USer alredy exists"
+        })
+    }
+
+    const hashPassword = await bcrypt.hash(password,10);
+    const user = new User({
+        fullName,
+        email,
+        password:hashPassword
+    })
+
+    await user.save();
+    const accessToken = jwt.sign(
+        {userId: user._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h"
+        }
+    );
+
+    return res.status(201).json({
+        error: false,
+        user:{ fullName:user.fullName, email:user.email },
+        accessToken,
+        message:"Registration Successful"
+    })
+})
+
+//Login
+app.post("/login", async(req,res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({
+            message:"Email and Password are required"
+        })
+    }
+
+    const user =  await User.findOne({email});
+    if(!user){
+        return res.status(400).json({
+            message:"User Not found"
+        });
+    }
+    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message:"Invalid Credentails"
+        })
+    }
+
+    const accessToken = jwt.sign(
+        {userId: user._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h"
+        }
+    );
+
+    return res.status(200).json({
+        error: false,
+        message : "Login Successful",
+        user: {fullName: user.fullName, email:user.email},
+        accessToken,
+    });
+
 })
 
 app.listen(8000);

ba4106ef59fb42a44c762ca3bd6e4f2b50673a30
Saurabh Yadav <yadavsaurabhyd@gmail.com>
2026-01-26 09:21:42 +0530

    tailwind add and login page

--- Frontend/src/pages/Auth/Login.jsx
+++ Frontend/src/pages/Auth/Login.jsx
@@ -7,13 +7,13 @@ const Login = () => {
         <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
         
           <div>
-            <h4 className='text-[15px] text-white leading-6 pr-7 mt-4'> Capture Your <br/> Journeys</h4>
-            <p className=''>
+            <h4 className="text-5xl text-white font-semibold leading-[58px]"> Capture Your <br/> Journeys</h4>
+            <p className='text-[15px] text-white leading-6 pr-7 mt-4'>
               Record your travel experience and memories in your personal travel journal. 
             </p>
           </div>
         </div>
-        <div className=''>
+        <div className='w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20'>
           <form onSubmit={()=>{}}>
             <h4 className='text-2xl font-semibold mb-7'>Login</h4>
             <input type='text' placeholder='Email' className='input-box'/>
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,37 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),
-//     tailwindcss(),
-//   ],
-//   theme:{
-//     fontFamily:{
-//       display:["Poppins", "sans-serif"]
-//     },
-//     extend:{
-//       colors:{
-//         primary: "#05B6D3",
-//         secondary: "#EF863E"
-//       },
-//       backgroundImage:{
-//         'login-bg-img':"url('./src/assets/images/bg-image.png')",
-//         'signup-bg-img':"url('./src/assets/images/signup-bg-img.png')",
-//       }
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
 })
